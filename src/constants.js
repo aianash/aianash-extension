745f38fc054dab976b6c55b945107df4a8089093
Neeraj Gangwar <y.neeraj2008@gmail.com>
2016-09-05 09:48:01 +0530

    update: icons and color

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 const NODE_PREFIX      = 'aianash'
 const AIANASH_CLASS    = 'aianash'
-const TAGS_ADD_URL     = 'http://aianash.com:9000/page-tags/add'
-const TAGS_GET_URL     = 'http://aianash.com:9000/page-tags/get'
-const TOKEN_ID_GET_URL = 'http://aianash.com:9000/token-id/get'
+const TAGS_ADD_URL     = 'http://aianash.com/api/page-tags/add'
+const TAGS_GET_URL     = 'http://aianash.com/api/page-tags/get'
+const TOKEN_ID_GET_URL = 'http://aianash.com/api/token-id/get'
 
 const OVERLAY = {
   ID               : 'aianash-overlay',
